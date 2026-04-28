dc7803bfdeb214d8c4f37215349173de198f43b2
Aamir Husain <aamir.husain@verizon.com>
2026-04-28 11:15:12 -0400

    complete code intro

---
+++ src/16_exercise.kt
@@ -0,0 +1,14 @@
+fun main() {
+    val customers = hashSetOf<String>(
+        "Liam",
+        "Harry",
+        "Louis",
+        "Zayn"
+    )
+
+    customers.add("Niall")
+    println("Customers: $customers")
+
+    customers.remove("Zayn")
+    println("Customers: $customers")
+}
---
+++ src/20_when_exercise.kt
@@ -0,0 +1,13 @@
+fun main() {
+    val name = "Bob"
+
+    // Get first letter of name
+    val firstLetter = name[0].lowercase()
+
+    // Print greeting
+    when (firstLetter) {
+        "a", "b", "c" -> println("Salutations")
+        "d", "e", "f" -> println("Ello m8")
+        else -> println("Oi!")
+    }
+}
